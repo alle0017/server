8c0b53b41c082c234c9ca5c21e2e9fd09c775d3d
alle0017 <124385256+alle0017@users.noreply.github.com>
2024-05-30 07:07:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const path = require('path');
-const puppeteer = require('puppeteer-extra');
-const stealthPlugin = require('puppeteer-extra-plugin-stealth');
+const puppeteer = require('puppeteer')
 const fs = require('fs');
-const antibot = require('./serverside-antibot.js');
-const bodyParser = require('body-parser');
-
- 
-
-
-puppeteer.use(stealthPlugin());
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
-console.clear();
-console.log('\x1B[1m\x1B[4mSTICKER LIVE SERVER\x1B[0m \x1B[1m\x1B[2m/|\\ ^._.^ /|\\ \x1B[0m\n\n');
-
-const log = ( str, color = { r: 0, g: 0, b: 0 } )=>{
-  console.log(`\x1B[1m[server]\x1B[0m \x1B[38;2;${color.r};${color.g};${color.b}m${str}\x1b[0m`);
-}
 
 const newPuppetChrome = async ( url )=>{
     const browser = await puppeteer.launch({ 
@@ -29,14 +12,10 @@ const newPuppetChrome = async ( url )=>{
       args:['--start-maximized' ],
       defaultViewport: null,
     });
-  
-    let [page] = await browser.pages();
-    if( !page )
-      page = await browser.newPage();
+    const page = await browser.newPage();
     await page.goto( url );
-  
     fs.watch(path.join(__dirname, '../'), {
-      recursive: false,
+      recursive: true,
     }, ()=>{
         page.reload()
     })
@@ -49,56 +28,18 @@ app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       // Set Cross-Origin-Opener-Policy header
-      //res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
       // Set Cross-Origin-Embedder-Policy header
-      //res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+      res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
       next();
 });
 app.use(express.static(path.join(__dirname, '../')));
 
-antibot.setupAntiBot(app, path)
-
 // Start the server
-const server = app.listen(PORT, async () => {
-      
-      log("opening web page with chrome...", {
-        r: 51,
-        g: 204,
-        b: 255,
-      })
-
-      const browser = await newPuppetChrome(`http://localhost:${PORT}`)
-      const close = () => {
-        console.clear();
-        log('server closed', { r: 255, g: 10, b: 50 })
-        browser.close();
-        server.close();
-      }
-
-      log(`server is running on port ${PORT}`,{
-        r: 51,
-        g: 204,
-        b: 255,
-      });
-      log(`http://localhost:${PORT}`, {
-        r: 0,
-        g: 102,
-        b: 255,
-      })
-      log('watching for file changes...', {
-        r: 102,
-        g: 0,
-        b: 255,
-      })
-
-      console.log('\n\x1B[1m\x1b[47m\x1B[38;2;255;0;255m PRESS ANY KEY TO EXIT \x1B[0m' );
-      process.on('SIGINT', close )
-      process.on('SIGQUIT', close );
-      process.on('SIGTERM', close );
-      process.on('exit', close );
-
-      process.stdin.on('data', close );
-      process.stdin.on('error', ()=>{
-        close();
-      })
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+      console.log(`[server] Server is running on port ${PORT}`);
+      console.log("[server] opening web page with chrome...")
+      console.log('[server] \x1b[34mhttp://localhost:3000\x1b[0m' );
+      newPuppetChrome("http://localhost:3000")
 });
